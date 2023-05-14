bd81c33bb38819caa2154e4a4e960b0ee34c99b8
SAMPILAHY HERINIAVO Tiany <sampilahy.heriniavo@gmail.com>
2023-05-14 13:35:14 +0300

    scroll

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function scrollToBottom() {
     const target = document.querySelector('#bottom');
     target.scrollIntoView({ behavior: 'smooth' });
 }
+
+function scrollToNemesis() {
+    const target = document.querySelector('#nemesis');
+    target.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToHero() {
+    const target = document.querySelector('#hero');
+    target.scrollIntoView({ behavior: 'smooth' });
+}
