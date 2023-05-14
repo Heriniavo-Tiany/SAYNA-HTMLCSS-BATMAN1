396331e977b4d66152e876d1a21347e1f2a4f9f7
SAMPILAHY HERINIAVO Tiany <sampilahy.heriniavo@gmail.com>
2023-05-14 13:50:53 +0300

    close popup

--- script.js
+++ script.js
@@ -15,9 +15,13 @@ function scrollToHero() {
 
 const myButton = document.getElementById("myButton");
 const myPopup = document.getElementById("myPopup");
+const closeBtn = myPopup.querySelector(".close-btn");
 
 myButton.addEventListener("click", function(event) {
     event.preventDefault(); // prevent default behavior of button click
     myPopup.style.display = "block"; // display the popup
 });
 
+closeBtn.addEventListener("click", function(event) {
+    myPopup.style.display = "none";
+});
