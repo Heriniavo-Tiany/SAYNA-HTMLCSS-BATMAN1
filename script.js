68bfdfd56af4f1aabe795d8ba55a6f3f7aac4fb6
SAMPILAHY HERINIAVO Tiany <sampilahy.heriniavo@gmail.com>
2023-05-14 13:49:54 +0300

    popup

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ function scrollToHero() {
     const target = document.querySelector('#hero');
     target.scrollIntoView({ behavior: 'smooth' });
 }
+
+const myButton = document.getElementById("myButton");
+const myPopup = document.getElementById("myPopup");
+
+myButton.addEventListener("click", function(event) {
+    event.preventDefault(); // prevent default behavior of button click
+    myPopup.style.display = "block"; // display the popup
+});
+
